c72975dece146d2dbf7f5840f48b29869abdbbb4
Renato Holzlsauer <renato.holzlsauer@gmail.com>
2020-05-21 17:48:22 -0300

    Add files via upload

---
+++ ex_01.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h> // sleep()
+
+int main(){    
+    for(int i = 0; i < 101; i++) {
+        printf(" %d\r", i);
+        fflush(stdout); // Some magic happens here, and...
+        sleep(0.5);
+        // O delay é pequeno demais para ver o números usando carriage return \r
+    }
+}
